46bde183b42edfe4cf516a8a24f2ea4967c0f99a
Vince <50725598+cvincentcoleman@users.noreply.github.com>
2022-07-27 08:19:04 -0400

    Initial Commit

---
+++ Memorize2/Memorize2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2App.swift
+//  Memorize2
+//
+//  Created by Vince Coleman on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
